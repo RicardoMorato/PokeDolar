b13bf72067d1dca2e01f2e2ada0d46ecc6f2b8d3
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2021-01-11 23:09:18 -0300

    feat: create project theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+  color: {
+    black: "#000",
+    white: "#fff",
+    red: "#EF5350",
+    blue: "#3B4CCA",
+    yellow: "#FECB36",
+    whiteHover: "#F0F0F0",
+    redHover: "#CC0000",
+  },
+  font: {
+    bigText: `normal normal bold 32px "Roboto", sans-serif`, // Roboto bold 32px: For titles
+    mediumText: `normal normal bold 18px "Roboto", sans-serif`, // Roboto bold 18px: For subtitles and other featured texts
+    normalText: `normal normal normal 16px "Roboto", sans-serif`, // Roboto regular 16px: For navbar, placeholder's and some components description
+    featuredNormalText: `normal normal bold 16px "Roboto", sans-serif`, // Roboto regular 16px: For secondary featured texts, like doctor name on "Meus pacientes" component
+    bodyText: `normal normal normal 14px "Roboto", sans-serif`, // Roboto regular 14px: For body texts
+    featuredText: `normal normal bold 14px "Roboto", sans-serif`, // Roboto regular 14px: For featured body texts
+    smallText: `normal normal normal 12px "Roboto", sans-serif`, // Roboto regular 12px: For main cards descriptions Ex: Dar feedback ao encaminhador,
+  },
+};
+
+export default theme;
